b598f2d8df1b669cc9dfa3d5322175944fdd7b43
Parid3 <manzoparide@gmail.com>
2023-04-06 11:58:49 +0200

    sviluppo 1 semaforo rosso

---
+++ EdCIvica/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace EdCivica
+{
+    public class Program
+    {
+        static void main(string[] args)
+        {
+            
+        }
+        public static string funzioneuno()
+        {
+            return "";
+        }
+    }
+
+
+}
